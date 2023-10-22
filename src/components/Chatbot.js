ae19e103dcc71b08c467a76ba75f05e7d093dbed
Hteam121 <Shaikhatim3@gmail.com>
2023-10-22 02:34:40 -0500

    after firebase works

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,18 +1,30 @@
 // src/components/Chatbot.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { sendMessage, listenForMessages } from '../services/ChatService';
+import '../static/ChatBot.css';  // Updated to match the actual file name
 
 const chatId = 'your_chat_id';  // Replace with your chat ID
 
 function Chatbot() {
   const [messages, setMessages] = useState([]);
   const [userInput, setUserInput] = useState('');
+  const chatAreaRef = useRef(null);
+
 
   useEffect(() => {
     listenForMessages(chatId, message => {
+      console.log("Received message:", message);
       setMessages(prevMessages => [...prevMessages, { ...message, sender: message.sender }]);
     });
   }, []);
+  
+  useEffect(() => {
+    if (chatAreaRef.current) {
+      chatAreaRef.current.scrollTop = chatAreaRef.current.scrollHeight;
+    }
+  }, [messages]);
+  
+  
 
   const handleSendMessage = () => {
     if (userInput.trim()) {
@@ -33,9 +45,12 @@ function Chatbot() {
 
   return (
     <div className="chatbot">
-      <div className="chat-area">
+      <div className="chat-area" ref={chatAreaRef}>
         {messages.map((message, index) => (
-          <div key={index} className={message.sender}>
+          <div 
+            key={index} 
+            className={`message ${message.sender === 'user' ? 'user' : 'other'}`}
+          >
             {message.text}
           </div>
         ))}
--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -3,15 +3,18 @@ import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 import { app } from './Firebase.js';  // Adjust the import to reflect the exported app object
 
 export function sendMessage(chatId, sender, text) {
-  const db = getDatabase(app);  // Adjust this line to use the app object
+  console.log('sendMessage called', chatId, sender, text);
+  const db = getDatabase(app);
   const newMessageRef = push(ref(db, `chats/${chatId}`));
   set(newMessageRef, { sender, text });
 }
 
 export function listenForMessages(chatId, callback) {
-  const db = getDatabase(app);  // Adjust this line to use the app object
+  console.log('listenForMessages called', chatId);
+  const db = getDatabase(app);
   const messagesRef = ref(db, `chats/${chatId}`);
   onChildAdded(messagesRef, snapshot => {
+    console.log('New message received', snapshot.val());
     callback(snapshot.val());
   });
 }
